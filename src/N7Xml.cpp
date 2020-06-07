eb5613fb8bbf45643e211147f39f106ea9ff573d
Alwin Esch <alwin.esch@web.de>
2020-06-07 04:51:17 +0200

    change to new C++ interface

--- src/N7Xml.cpp
+++ src/N7Xml.cpp
@@ -8,150 +8,207 @@
 
 #include "N7Xml.h"
 
-#include <p8-platform/util/StringUtils.h>
-#include "tinyxml.h"
-#include "util/XMLUtils.h"
+#include <kodi/Filesystem.h>
+#include <tinyxml.h>
 
-using namespace ADDON;
-
-bool CCurlFile::Get(const std::string &strURL, std::string &strResult)
+bool CCurlFile::Get(const std::string& strURL, std::string& strResult)
 {
-  void* fileHandle = XBMC->OpenFile(strURL.c_str(), 0);
-  if (fileHandle)
+  kodi::vfs::CFile fileHandle;
+  if (fileHandle.OpenFile(strURL, 0))
   {
-    char buffer[1024];
-    while (XBMC->ReadFileString(fileHandle, buffer, 1024))
+    std::string buffer;
+    while (fileHandle.ReadLine(buffer))
       strResult.append(buffer);
-    XBMC->CloseFile(fileHandle);
     return true;
   }
   return false;
 }
 
-N7Xml::N7Xml(void) :
-        m_connected(false)
+N7Xml::N7Xml()
 {
+  kodi::Log(ADDON_LOG_DEBUG, "Creating N7 PVR-Client");
+
+  m_strHostname = kodi::GetSettingString("n7host", DEFAULT_HOST);
+  m_iPort = kodi::GetSettingInt("n7port", DEFAULT_PORT);
+
   list_channels();
 }
 
-N7Xml::~N7Xml(void)
+ADDON_STATUS N7Xml::SetSetting(const std::string& settingName,
+                               const kodi::CSettingValue& settingValue)
 {
-  m_channels.clear();
+  if (settingName == "n7host")
+  {
+    std::string tmp_sHostname;
+    kodi::Log(ADDON_LOG_INFO, "Changed Setting 'host' from %s to %s", m_strHostname.c_str(),
+              settingValue.GetString().c_str());
+    tmp_sHostname = m_strHostname;
+    m_strHostname = settingValue.GetString();
+    if (tmp_sHostname != m_strHostname)
+      return ADDON_STATUS_NEED_RESTART;
+  }
+  else if (settingName == "n7port")
+  {
+    kodi::Log(ADDON_LOG_INFO, "Changed Setting 'port' from %u to %u", m_iPort,
+              settingValue.GetInt());
+    if (m_iPort != settingValue.GetInt())
+    {
+      m_iPort = settingValue.GetInt();
+      return ADDON_STATUS_NEED_RESTART;
+    }
+  }
+  return ADDON_STATUS_OK;
 }
 
-int N7Xml::getChannelsAmount()
-{ 
-  return m_channels.size();
+PVR_ERROR N7Xml::GetCapabilities(kodi::addon::PVRCapabilities& capabilities)
+{
+  capabilities.SetSupportsTV(true);
+  capabilities.SetSupportsRecordings(false);
+  capabilities.SetSupportsRecordingsRename(false);
+  capabilities.SetSupportsRecordingsLifetimeChange(false);
+  capabilities.SetSupportsDescrambleInfo(false);
+
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetBackendName(std::string& name)
+{
+  name = "NJoy N7";
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetBackendVersion(std::string& version)
+{
+  version = "-";
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetConnectionString(std::string& connection)
+{
+  connection = "connected";
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetBackendHostname(std::string& hostname)
+{
+  hostname = m_strHostname;
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetChannelsAmount(int& amount)
+{
+  amount = m_channels.size();
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetChannels(bool radio, kodi::addon::PVRChannelsResultSet& results)
+{
+  if (m_connected)
+  {
+    for (const auto& channel : m_channels)
+    {
+      kodi::Log(ADDON_LOG_DEBUG, "N7Xml - Loaded channel - %s.",
+                channel.second.GetChannelName().c_str());
+      results.Add(channel.second);
+    }
+  }
+  else
+  {
+    kodi::Log(ADDON_LOG_DEBUG, "N7Xml - no channels loaded");
+  }
+
+  return PVR_ERROR_NO_ERROR;
+}
+
+PVR_ERROR N7Xml::GetChannelStreamProperties(const kodi::addon::PVRChannel& channel,
+                                            std::vector<kodi::addon::PVRStreamProperty>& properties)
+{
+  for (const auto& chan : m_channels)
+  {
+    if (chan.second.GetUniqueId() == channel.GetUniqueId())
+    {
+      properties.emplace_back(PVR_STREAM_PROPERTY_STREAMURL, chan.first);
+      properties.emplace_back(PVR_STREAM_PROPERTY_ISREALTIMESTREAM, "true");
+      return PVR_ERROR_NO_ERROR;
+    }
+  }
+
+  return PVR_ERROR_UNKNOWN;
 }
 
 void N7Xml::list_channels()
 {
-  std::string strUrl;
-  strUrl = StringUtils::Format("http://%s:%i/n7channel_nt.xml", g_strHostname.c_str(), g_iPort);
+  std::string strUrl =
+      "http://" + m_strHostname + ":" + std::to_string(m_iPort) + "/n7channel_nt.xml";
   std::string strXML;
 
   CCurlFile http;
-  if(!http.Get(strUrl, strXML))
+  if (!http.Get(strUrl, strXML))
   {
-    XBMC->Log(LOG_DEBUG, "N7Xml - Could not open connection to N7 backend.");
+    kodi::Log(ADDON_LOG_DEBUG, "N7Xml - Could not open connection to N7 backend.");
+    kodi::addon::CInstancePVRClient::ConnectionStateChange("", PVR_CONNECTION_STATE_SERVER_UNREACHABLE, "");
   }
   else
   {
     TiXmlDocument xml;
     xml.Parse(strXML.c_str());
     TiXmlElement* rootXmlNode = xml.RootElement();
-    if (rootXmlNode == NULL)
+    if (rootXmlNode == nullptr)
       return;
     TiXmlElement* channelsNode = rootXmlNode->FirstChildElement("channel");
     if (channelsNode)
     {
-      XBMC->Log(LOG_DEBUG, "N7Xml - Connected to N7 backend.");
+      kodi::Log(ADDON_LOG_DEBUG, "N7Xml - Connected to N7 backend.");
       m_connected = true;
       int iUniqueChannelId = 0;
-      TiXmlNode *pChannelNode = NULL;
-      while ((pChannelNode = channelsNode->IterateChildren(pChannelNode)) != NULL)
+      TiXmlNode* pChannelNode = nullptr;
+      while ((pChannelNode = channelsNode->IterateChildren(pChannelNode)) != nullptr)
       {
-        std::string strTmp;
-        PVRChannel channel;
+        kodi::addon::PVRChannel channel;
 
         /* unique ID */
-        channel.iUniqueId = ++iUniqueChannelId;
+        channel.SetUniqueId(++iUniqueChannelId);
 
         /* channel number */
-        if (!XMLUtils::GetInt(pChannelNode, "number", channel.iChannelNumber))
-          channel.iChannelNumber = channel.iUniqueId;
+        const TiXmlNode* pNode = pChannelNode->FirstChild("number");
+        if (pNode && pNode->FirstChild())
+          channel.SetChannelNumber(atoi(pNode->FirstChild()->Value()));
+        else
+          channel.SetChannelNumber(channel.GetUniqueId());
 
         /* channel name */
-        if (!XMLUtils::GetString(pChannelNode, "title", strTmp))
+        const TiXmlElement* pElement = pChannelNode->FirstChildElement("title");
+        if (pElement && (pNode = pElement->FirstChild()))
+          channel.SetChannelName(pNode->Value());
+        else
           continue;
-        channel.strChannelName = strTmp;
 
         /* icon path */
-        const TiXmlElement* pElement = pChannelNode->FirstChildElement("media:thumbnail");
-        channel.strIconPath = pElement->Attribute("url");
+        pElement = pChannelNode->FirstChildElement("media:thumbnail");
+        channel.SetIconPath(pElement->Attribute("url"));
 
         /* channel url */
-        if (!XMLUtils::GetString(pChannelNode, "guid", strTmp))
-          channel.strStreamURL = "";
-        else
-          channel.strStreamURL = strTmp;
+        std::string url;
+        pElement = pChannelNode->FirstChildElement("media:content");
+        if (pElement)
+        {
+          url = pElement->Attribute("url");
+          channel.SetMimeType(pElement->Attribute("type"));
+        }
 
-        m_channels.push_back(channel);
+        if (url.empty())
+        {
+          pElement = pChannelNode->FirstChildElement("guid");
+          if (pElement && (pNode = pElement->FirstChild()))
+            url = pNode->Value();
+        }
+
+        m_channels.emplace_back(url, channel);
       }
     }
+
+    kodi::addon::CInstancePVRClient::ConnectionStateChange("", PVR_CONNECTION_STATE_CONNECTED, "");
   }
 }
 
-
-PVR_ERROR N7Xml::requestChannelList(ADDON_HANDLE handle, bool bRadio)
-{
-  if (m_connected)
-  {
-    std::vector<PVRChannel>::const_iterator item;
-    PVR_CHANNEL tag;
-    for( item = m_channels.begin(); item != m_channels.end(); ++item)
-    {
-      const PVRChannel& channel = *item;
-      memset(&tag, 0 , sizeof(tag));
-
-      tag.iUniqueId       = channel.iUniqueId;
-      tag.iChannelNumber  = channel.iChannelNumber;
-      strncpy(tag.strChannelName, channel.strChannelName.c_str(), sizeof(tag.strChannelName) - 1);
-      strncpy(tag.strIconPath, channel.strIconPath.c_str(), sizeof(tag.strIconPath) - 1);
-
-      XBMC->Log(LOG_DEBUG, "N7Xml - Loaded channel - %s.", tag.strChannelName);
-      PVR->TransferChannelEntry(handle, &tag);
-    }
-  }
-  else
-  {
-    XBMC->Log(LOG_DEBUG, "N7Xml - no channels loaded");
-  }
-
-  return PVR_ERROR_NO_ERROR;
-}
-
-PVR_ERROR N7Xml::GetChannelStreamProperties(const PVR_CHANNEL* channel, PVR_NAMED_VALUE* props, unsigned int* prop_size)
-{
-  if (*prop_size < 2)
-    return PVR_ERROR_INVALID_PARAMETERS;
-
-  if (!m_channels.empty())
-  {
-    std::vector<PVRChannel>::const_iterator item;
-    for (item = m_channels.begin(); item != m_channels.end(); ++item)
-    {
-       const PVRChannel& chan = *item;
-       if (chan.iUniqueId == channel->iUniqueId)
-       {
-         strncpy(props[0].strName, PVR_STREAM_PROPERTY_STREAMURL, sizeof(props[0].strName) - 1);
-         strncpy(props[0].strValue, chan.strStreamURL.c_str(), sizeof(props[0].strValue) - 1);
-         strncpy(props[1].strName, PVR_STREAM_PROPERTY_ISREALTIMESTREAM, sizeof(props[1].strName) - 1);
-         strncpy(props[1].strValue, "true", sizeof(props[1].strValue) - 1);
-         *prop_size = 2;
-         return PVR_ERROR_NO_ERROR;
-       }
-    }
-  }
-  return PVR_ERROR_UNKNOWN;
-}
+ADDONCREATOR(N7Xml)
